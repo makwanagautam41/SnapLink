7e71a9e7c63e10ae3dbad4e1873a1c5c10b37e5e
Gautam Makwana <makwanagautam411@gmail.com>
2025-05-06 20:10:23 +0530

    none

--- client/src/components/AllComments.jsx
+++ client/src/components/AllComments.jsx
@@ -219,7 +219,7 @@ const AllComments = ({
             }}
           />
 
-          <button
+          {/* <button
             onClick={() => {
               handlePostComment(post._id);
               setShowEmojiPicker(false);
@@ -230,6 +230,19 @@ const AllComments = ({
                 ? styles.text
                 : "cursor-not-allowed text-gray-400"
             }`}
+          >
+            {isCommenting ? (
+              <Icon.Loader className="animate-spin w-6 h-6" />
+            ) : (
+              "Post"
+            )}
+          </button> */}
+          <button
+            onClick={() => {
+              handlePostComment(post._id);
+              setShowEmojiPicker(false);
+            }}
+            className={`ml-2 px-4 py-2 rounded-md cursor-pointer ${styles.text}`}
           >
             {isCommenting ? (
               <Icon.Loader className="animate-spin w-6 h-6" />
